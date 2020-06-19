feadd34812cf8c56fd08cb7e86558fee452e20ae
Amanda Haldy <aehaldy@tutanota.com>
2020-06-19 00:19:36 -0400

    FEATURE: connects Flask to React and fetches API data

--- api/api.py
+++ api/api.py
@@ -1,7 +1,20 @@
 from flask import Flask
+import requests
+import json
 
 app = Flask(__name__)
 
-@app.route('/uwm')
-def get_stats():
-  return 'Welcome to University of Wisonsin, Madison, STATS!n'
+
+@app.route('/uwm', methods=['GET'])
+def get_uwm_stats():
+    try:
+      #Request data from API endpoint
+      req = requests.get('https://api.data.gov/ed/collegescorecard/v1/schools/?school.operating=1&2015.academics.program_available.assoc_or_bachelors=true&2015.student.size__range=1..&school.degrees_awarded.predominant__range=1..3&school.degrees_awarded.highest__range=2..4&id=240444&api_key=MkI1iM1B22ezc4GpLEdgNCe89ZxkwEMCgBQgQRzY')
+
+      #Convert the data into a JSON object
+      data = json.loads(req.content)
+      return data
+
+    except Exception as e:
+        print('Failed to GET data from API')
+        return(str(e))
--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [schoolName, setSchoolName] = useState(null);
+  const [schoolAlias, setSchoolAlias] = useState(null);
+  const [schoolUrl, setSchoolUrl] = useState(null);
+  const [city, setCity] = useState(null);
+  const [state, setState] = useState(null);
+  const [zip, setZip] = useState(null);
+  const [studentPop, setStudentPop] = useState(null);
+
+  useEffect( () => {
+    const fetchStats = async () => {
+      //fetch data from Flask API
+      const res = await fetch('/uwm');
+      const data = await res.json();
+      console.log('Got data?', data);
+
+      //assign school stats to state
+      const school = data.results[0].school;
+      const latestStats = data.results[0].latest;
+      setSchoolName(school.name);
+      setSchoolAlias(school.alias);
+      setSchoolUrl(school.school_url);
+      setCity(school.city);
+      setState(school.state);
+      setZip(school.zip);
+      setStudentPop(
+        latestStats.student.enrollment.grad_12_month +
+        latestStats.student.enrollment.undergrad_12_month
+      );
+    }
+    fetchStats();
+  }, []);
+
   return (
+    //API request takes a min, so show a loading gif or something
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{schoolName} "{schoolAlias}"</p>
+        <a href={`http://${schoolUrl}`}>{schoolUrl}</a>
+        <p> {city}, {state} {zip}</p>
+        <p>{studentPop} graduates and undergranduates combined</p>
       </header>
     </div>
   );
